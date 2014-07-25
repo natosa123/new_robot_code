fc676914e9158cf3f62eb667c3ca5d7b22bed3a8
nathan <davidosachoff@dhcp-128-189-193-218.ubcsecure.wireless.ubc.ca>
2014-07-24 18:08:33 -0700

    changed some stuff in followTape

--- followTape.h
+++ followTape.h
@@ -6,56 +6,6 @@
 //
 //
 
-//****TAKE OUT VARIABLES DECLARATIONS AND MOVE TO MAIN FUNCTION SO THEY ARE ONLY DECLARDED ONCE*******
-//***START COPYING******
-
-
-// Tape following Pins
-int leftTapeQRD = 3;
-int rightTapeQRD = 2;
-
-//thresholds for tape following QRDs
-int leftThreshold = 485;
-int rightThreshold = 120;
-
-//idol detector (qrd) pin
-int idolSensorPin = 1;
-
-//threshold for idol sensor
-int idolSensorThreshold = 300 ;
-
-// Motor Speed Pins (motor outputs)
-int rightWheelPin = 1;
-int leftWheelPin = 0;
-
-//Knobs
-int knob_one = 6;
-int knob_two = 7;
-
-//PID parameters
-int kP = 100;
-int kD = 20;
-
-//correction errors (negative is too far left, positive is too far right)
-int error = 0; 
-int lasterror = 0;
-int preverrorDuration = 0;
-int timeSinceDiffErr = 1;
-
-//turning parameters
-float motorAdjustment = 0.5;
-int rightMotorOutput = 0;
-int leftMotorOutput = 0;
-int PIDAdjustment = 0;
-
-// wheel controll
-int startMotorSpeed = 375;
-int motorSpeed = 150;
-int sharpTurnSpeed = 100;
-int neutral = 94;
-int servoPosition = neutral;
-//****END COPYING****
-
 #ifndef _followTape_h
 #define _followTape_h
 
